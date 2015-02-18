9cfeb8651723dc1a4a98400d2ca658b4f66def5c
Robbie Pallas <rpallas@gmail.com>
2015-02-18 00:33:39 +0000

    added basic hapi proxy server

---
+++ index.js
@@ -0,0 +1,23 @@
+var Hapi = require('hapi');
+var Wreck = require('wreck');
+
+var server = new Hapi.Server();
+server.connection({ port: 9999 });
+
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    console.log('request: ', request);
+    Wreck.get(request.path, function (err, res, payload) {
+      if (err) {
+        console.error(err);
+      }
+      reply(payload);
+    });
+  }
+});
