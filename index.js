84b1265b2e5a277aeb91fa62bb8f7ebb65860318
Robbie Pallas <rpallas@gmail.com>
2015-03-21 01:51:38 +0000

    use http-proxy

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-var Hapi = require('hapi');
-var Wreck = require('wreck');
+var util = require('util'),
+    colors = require('colors'),
+    http = require('http'),
+    httpProxy = require('http-proxy');
 
-var server = new Hapi.Server();
-server.connection({ port: 9999 });
+//require('colors');
 
-server.start(function () {
-  console.log('Server running at:', server.info.uri);
-});
+var welcome = [
+  '#    # ##### ##### #####        #####  #####   ####  #    # #   #',
+  '#    #   #     #   #    #       #    # #    # #    #  #  #   # # ',
+  '######   #     #   #    # ##### #    # #    # #    #   ##     #  ',
+  '#    #   #     #   #####        #####  #####  #    #   ##     #  ',
+  '#    #   #     #   #            #      #   #  #    #  #  #    #  ',
+  '#    #   #     #   #            #      #    #  ####  #    #   #  '
+].join('\n');
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    console.info('request: ', request.path);
-    Wreck.get(request.path, function (err, res, payload) {
-      if (err) {
-        console.error(err);
-      }
-      reply(payload);
-    });
-  }
-});
+util.puts(welcome.rainbow.bold);
+
+//
+// Basic Http Proxy Server
+//
+httpProxy.createServer({
+  target:'http://localhost:9003'
+}).listen(8003);
+
+//
+// Target Http Server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9003);
+
+util.puts('http proxy server'.blue + ' started '.green.bold + 'on port '.blue + '8003'.yellow);
+util.puts('http server '.blue + 'started '.green.bold + 'on port '.blue + '9003 '.yellow);
